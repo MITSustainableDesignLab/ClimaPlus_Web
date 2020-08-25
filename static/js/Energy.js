9547f3b9c30297a1352d2b3886a9b6647fc324e8
AlphaArsano <aarsano@mit.edu>
2020-08-25 13:15:59 -0400

    0825_GUI Latest

--- static/js/Energy.js
+++ static/js/Energy.js
@@ -129,6 +129,7 @@ var initSavedBarChart = {
     title = config.title,
     color = config.color,
     first = config.first,
+    last = config.last,
     max = config.max,
     margin = {top: 20, right: 20, bottom: 50, left: 50},
     parseDate = d3.timeParse("%m/%Y"),
@@ -203,7 +204,7 @@ var initSavedBarChart = {
         .attr("y", -30)
         .style("text-anchor", "end")
         .text(title);
-
+        }
     //   svg.append("g")
     //   .attr("class", "y axis")
     //   .call(xAxis)
@@ -214,34 +215,36 @@ var initSavedBarChart = {
     //   .style("text-anchor", "end")
     //   .text(title);
 
-      // draw legend
-      var legend = svg.selectAll(".legend")
-          .data(stackKey)
-        .enter().append("g")
-          .attr("class", "legend")
-          .attr("transform", function(d, i) { return "translate(0," + i * 15 + ")"; });
+    if (last){
+          // draw legend
+          var legend = svg.selectAll(".legend")
+              .data(stackKey)
+            .enter().append("g")
+              .attr("class", "legend")
+              .attr("transform", function(d, i) { return "translate(50," + (i * 15 -62)  + ")"; });
 
-      // draw legend colored rectangles
-      legend.append("rect")
-          .attr("x", 0) //.attr("x", width - 18)
-          .attr("y", height+8)
-          .attr("width", 12) //.attr("width", 12)
-          .attr("height", 8)
-          .style("fill", color);
+          // draw legend colored rectangles
+          legend.append("rect")
+              .attr("x", 0) //.attr("x", width - 18)
+              .attr("y", height+8)
+              .attr("width", 12) //.attr("width", 12)
+              .attr("height", 8)
+              .style("fill", color);
 
-      // draw legend text
-      legend.append("text")
-          .attr("x", 15)
-          .attr("y", height+13)
-          .attr("dy", ".15em")
-          // .style("text-anchor", "end")
-          .style("font-size","9.5px")
-          .style("font-color","grey")
-          .text(function(d) { return d;})
-    }
-   }
+          // draw legend text
+          legend.append("text")
+              .attr("x", 15)
+              .attr("y", height+13)
+              .attr("dy", ".15em")
+              // .style("text-anchor", "end")
+              .style("font-size","9.5px")
+              .style("font-color","grey")
+              .text(function(d) { return d;})
+          }
 
   }
+
+}
 
 var data = [
 {"Heating":Qh_T, "Cooling":Qc_T, "Equipment":Qe_T, "Lighting":Ql_T,"total":EUI}
@@ -264,7 +267,7 @@ colorL = {"Equipment": "black", "Lighting": "yellow","Cooling":"#62749e","Heatin
 colorL_ = {"Electricity":"darkgrey","Gas":"orange"}
 
 
-function newBase(cookiedata, svgid, first, max){
+function newBase(cookiedata, svgid, first, max, last){
     var svg = d3.select(svgid);
     svg.selectAll("*").remove();
 //    if (first == 1){
@@ -288,7 +291,8 @@ function newBase(cookiedata, svgid, first, max){
           title:"Energy Use (kWh/m2)",
           color: colorL,
           max: max,
-          first: first
+          first: first,
+          last: last
         });
 //    }
 }
